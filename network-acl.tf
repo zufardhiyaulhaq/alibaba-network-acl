bc7da5772b991dc183d41a6fd687ab39a7dee334
Zufar Dhiyaullhaq <zufardhiyaulhaq@gmail.com>
2024-07-24 13:06:23 +0700

    feat: update acl ingress rules
    Signed-off-by: Zufar Dhiyaullhaq <zufardhiyaulhaq@gmail.com>

--- network-acl.tf
+++ network-acl.tf
@@ -4,12 +4,38 @@ locals {
   allow_all_from_application = formatlist("ACCEPT#%s#ALL#ALL", var.application_subnets_cidr)
   allow_all_from_stateful    = formatlist("ACCEPT#%s#ALL#ALL", var.stateful_subnets_cidr)
   allow_all_From_compliance  = formatlist("ACCEPT#%s#ALL#ALL", var.compliance_subnets_cidr)
+
+  allow_22_tcp_from_utility     = formatlist("ACCEPT#%s#22#TCP", var.utility_subnets_cidr)
+  allow_3022_tcp_from_utility   = formatlist("ACCEPT#%s#22#TCP", var.utility_subnets_cidr)
+  allow_443_tcp_from_utility    = formatlist("ACCEPT#%s#80#TCP", var.utility_subnets_cidr)
+
+  allow_443_tcp_from_internet = "ACCEPT#0.0.0.0/0#443#TCP"
+  allow_80_tcp_from_internet = "ACCEPT#0.0.0.0/0#80#TCP"
+  allow_15012_tcp_from_internet = "ACCEPT#0.0.0.0/0#15012#TCP"
+  allow_15443_tcp_from_internet = "ACCEPT#0.0.0.0/0#15443#TCP"
+
+  allow_443_tcp_from_public = formatlist("ACCEPT#%s#443#TCP", var.public_subnets_cidr)
+  allow_80_tcp_from_public = formatlist("ACCEPT#%s#80#TCP", var.public_subnets_cidr)
+  allow_15012_tcp_from_public = formatlist("ACCEPT#%s#15012#TCP", var.public_subnets_cidr)
+  allow_15443_tcp_from_public = formatlist("ACCEPT#%s#15443#TCP", var.public_subnets_cidr)
+
+  allow_9099_tcp_from_internet = "ACCEPT#0.0.0.0/0#9099#TCP"
 }
 
 resource "tencentcloud_vpc_acl" "public" {
   vpc_id  = var.vpc_id
   name    = "public-network-acl"
-  ingress = concat(local.allow_all_from_public)
+  ingress = concat(
+    local.allow_all_from_public,
+    local.allow_22_tcp_from_utility,
+    local.allow_3022_tcp_from_utility,
+    local.allow_443_tcp_from_utility,
+    local.allow_443_tcp_from_internet,
+    local.allow_80_tcp_from_internet,
+    local.allow_15012_tcp_from_internet,
+    local.allow_15443_tcp_from_internet,
+    local.allow_9099_tcp_from_internet,
+    )
   egress = [
     "ACCEPT#0.0.0.0/0#ALL#ALL",
   ]
@@ -30,7 +56,9 @@ resource "tencentcloud_vpc_acl_attachment" "public" {
 resource "tencentcloud_vpc_acl" "utility" {
   vpc_id  = var.vpc_id
   name    = "utility-network-acl"
-  ingress = concat(local.allow_all_from_utility)
+  ingress = concat(
+    local.allow_all_from_utility,
+  )
   egress = [
     "ACCEPT#0.0.0.0/0#ALL#ALL",
   ]
@@ -51,7 +79,19 @@ resource "tencentcloud_vpc_acl_attachment" "utility" {
 resource "tencentcloud_vpc_acl" "application" {
   vpc_id  = var.vpc_id
   name    = "application-network-acl"
-  ingress = concat(local.allow_all_from_application)
+  ingress = concat(
+    local.allow_all_from_application,
+    local.allow_22_tcp_from_utility,
+    local.allow_3022_tcp_from_utility,
+    local.allow_443_tcp_from_utility,
+    # start temp
+    local.allow_443_tcp_from_internet,
+    # end temp
+    local.allow_443_tcp_from_public,
+    local.allow_80_tcp_from_public,
+    local.allow_15012_tcp_from_public,
+    local.allow_15443_tcp_from_public,
+  )
   egress = [
     "ACCEPT#0.0.0.0/0#ALL#ALL",
   ]
