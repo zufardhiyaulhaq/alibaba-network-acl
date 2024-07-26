0c78794bdfb64cb633f564e77bc396d17cd0892b
Zufar Dhiyaullhaq <zufardhiyaulhaq@gmail.com>
2024-07-26 18:17:40 +0700

    feat: add network acl
    Signed-off-by: Zufar Dhiyaullhaq <zufardhiyaulhaq@gmail.com>

--- variables.tf
+++ variables.tf
@@ -13,10 +13,10 @@ variable "vpc_id" {
   type        = string
 }
 
-# variable "public_subnets_cidr" {
-#   description = "List of public subnets CIDR"
-#   type        = list(string)
-# }
+variable "public_subnets_cidr" {
+  description = "List of public subnets CIDR"
+  type        = list(string)
+}
 
 variable "utility_subnets_cidr" {
   description = "List of utility subnets CIDR"
@@ -28,40 +28,40 @@ variable "application_subnets_cidr" {
   type        = list(string)
 }
 
-# variable "stateful_subnets_cidr" {
-#   description = "List of stateful subnets CIDR"
-#   type        = list(string)
-# }
+variable "stateful_subnets_cidr" {
+  description = "List of stateful subnets CIDR"
+  type        = list(string)
+}
 
-# variable "compliance_subnets_cidr" {
-#   description = "List of compliance subnets CIDR"
-#   type        = list(string)
-# }
+variable "compliance_subnets_cidr" {
+  description = "List of compliance subnets CIDR"
+  type        = list(string)
+}
 
-# variable "public_subnets_id" {
-#   description = "List of public subnets ID"
-#   type        = list(string)
-# }
+variable "public_subnets_id" {
+  description = "List of public subnets ID"
+  type        = list(string)
+}
 
-# variable "utility_subnets_id" {
-#   description = "List of utility subnets ID"
-#   type        = list(string)
-# }
+variable "utility_subnets_id" {
+  description = "List of utility subnets ID"
+  type        = list(string)
+}
 
 variable "application_subnets_id" {
   description = "List of application subnets ID"
   type        = list(string)
 }
 
-# variable "stateful_subnets_id" {
-#   description = "List of stateful subnets ID"
-#   type        = list(string)
-# }
+variable "stateful_subnets_id" {
+  description = "List of stateful subnets ID"
+  type        = list(string)
+}
 
-# variable "compliance_subnets_id" {
-#   description = "List of compliance subnets ID"
-#   type        = list(string)
-# }
+variable "compliance_subnets_id" {
+  description = "List of compliance subnets ID"
+  type        = list(string)
+}
 
 # variable "additional_ingress_public_rules" {
 #   description = "List of additional ingress rules for public network ACL"
