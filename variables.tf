ea6d00a52c774abad7d5515d3f1eefb7007a0ef1
Zufar Dhiyaullhaq <zufardhiyaulhaq@gmail.com>
2024-07-29 13:42:30 +0700

    feat: add network acl
    Signed-off-by: Zufar Dhiyaullhaq <zufardhiyaulhaq@gmail.com>

--- variables.tf
+++ variables.tf
@@ -66,10 +66,10 @@ variable "compliance_subnets_id" {
 variable "additional_ingress_application_rules" {
   description = "List of additional ingress rules for application subnet ACL"
   type = map(object({
+    name           = string
     policy         = number
     protocol       = string
     source_cidr_ip = string
-    name           = string
     policy         = number
   }))
 }
